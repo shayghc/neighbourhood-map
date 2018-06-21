d3d32edd54bf03f65d1fbdf692582f2320fde8e0
Seamus Connolly <sghconnolly@gmail.com>
2018-06-21 15:30:01 +0100

    feat: Add a marker to the map.

--- map-app/src/App.js
+++ map-app/src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
 export default class App extends React.Component {
-  componentDidMount() {
-    let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 50.7934612, lng: -1.1098803},
-      zoom: 14,
-      mapTypeId: 'roadmap',
-    });
-  }
+    componentDidMount() {
+        let map = new window.google.maps.Map(document.getElementById("map"), {
+            center: { lat: 50.7934612, lng: -1.1098803 },
+            zoom: 14,
+            mapTypeId: "roadmap"
+        });
+        // Markers
+        let spinnaker = { lat: 50.7956376, lng: -1.1085203 };
+        // Note the use of window here. This is necessary in React
+        let marker = new window.google.maps.Marker({
+            position: spinnaker,
+            map: map,
+            draggable: true,
+            title: 'Spinnaker Tower'
+        })
+    }
 
-  render() {
-    return (
-      <div id='app'>
-        <div id='map' />
-      </div>
-    );
-  }
-};
+    render() {
+        return (
+            <div id="app">
+                <div id="map" />
+            </div>
+        );
+    }
+}
