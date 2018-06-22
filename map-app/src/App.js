6fcc10dca69889f8fe0f8c19d5ffad8642b9b780
Seamus Connolly <sghconnolly@gmail.com>
2018-06-22 13:23:41 +0100

    feat: Page header added

--- map-app/src/App.js
+++ map-app/src/App.js
@@ -4,16 +4,67 @@ import "./index.css";
 export default class App extends React.Component {
     state = {
         locations: [
-            {title: 'Spinnaker Tower', location: {lat: 50.79557519999999, lng: -1.1085171}},
-            {title: 'HMS Warrior 1860', location: {lat: 50.7982384, lng: -1.1092475}},
-            {title: 'Round Tower', location: {lat: 50.7905406, lng: -1.1088802}},
-            {title: 'Mary Rose Museum', location: {lat: 50.8022114, lng: -1.1088526}},
-            {title: 'Action Stations', location: {lat: 50.79991680000001, lng: -1.1070106}},
-            {title: 'Charles Dickens Birthplace Museum', location: {lat: 50.8070676, lng: -1.0872127}},
-            {title: 'The D-Day Story', location: {lat: 50.77964100000001, lng: -1.089412}},
-            {title: 'Southsea Castle', location: {lat: 50.777995, lng: -1.0888283}},
-            {title: 'Royal Marines Museum', location: {lat: 50.78429, lng: -1.053795}},
-            {title: 'The Royal Navy Submarine Museum', location: {lat: 50.7881692, lng: -1.1195995}}
+            {
+                title: 'Spinnaker Tower',
+                location: {
+                    lat: 50.79557519999999,
+                    lng: -1.1085171
+                }
+            }, {
+                title: 'HMS Warrior 1860',
+                location: {
+                    lat: 50.7982384,
+                    lng: -1.1092475
+                }
+            }, {
+                title: 'Round Tower',
+                location: {
+                    lat: 50.7905406,
+                    lng: -1.1088802
+                }
+            }, {
+                title: 'Mary Rose Museum',
+                location: {
+                    lat: 50.8022114,
+                    lng: -1.1088526
+                }
+            }, {
+                title: 'Action Stations',
+                location: {
+                    lat: 50.79991680000001,
+                    lng: -1.1070106
+                }
+            }, {
+                title: 'Charles Dickens Birthplace Museum',
+                location: {
+                    lat: 50.8070676,
+                    lng: -1.0872127
+                }
+            }, {
+                title: 'The D-Day Story',
+                location: {
+                    lat: 50.77964100000001,
+                    lng: -1.089412
+                }
+            }, {
+                title: 'Southsea Castle',
+                location: {
+                    lat: 50.777995,
+                    lng: -1.0888283
+                }
+            }, {
+                title: 'Royal Marines Museum',
+                location: {
+                    lat: 50.78429,
+                    lng: -1.053795
+                }
+            }, {
+                title: 'The Royal Navy Submarine Museum',
+                location: {
+                    lat: 50.7881692,
+                    lng: -1.1195995
+                }
+            }
         ],
         markers: []
     }
@@ -22,7 +73,10 @@ export default class App extends React.Component {
         const {locations} = this.state
         const markersList = []
         let map = new window.google.maps.Map(document.getElementById("map"), {
-            center: { lat: 50.7934612, lng: -1.1098803 },
+            center: {
+                lat: 50.7934612,
+                lng: -1.1098803
+            },
             zoom: 12,
             mapTypeId: "roadmap"
         });
@@ -34,13 +88,7 @@ export default class App extends React.Component {
             let position = locations[i].location;
             let title = locations[i].title;
             // Create a location per marker
-            let marker = new window.google.maps.Marker({
-                map: map,
-                position: position,
-                title: title,
-                animation: window.google.maps.Animation.DROP,
-                id: title
-            })
+            let marker = new window.google.maps.Marker({map: map, position: position, title: title, animation: window.google.maps.Animation.DROP, id: title})
             // Push each marker to the markers array
             markersList.push(marker);
             // Extend the boundaries of the map for the Markers
@@ -69,10 +117,19 @@ export default class App extends React.Component {
     }
 
     render() {
-        return (
-            <div id="app">
-                <div id="map" />
-            </div>
+        let spanStyle = {
+            fontSize:"160px",
+            cursor:"pointer",
+            color:"white"
+        }
+        return (<div id="app">
+            <header>
+                <span style={{spanStyle}} onClick="openNav()">&#9776;</span>
+                <h1>Portsmouth POIs</h1>
+            </header>
+            
+            <div id="map"/>
+        </div>
         );
     }
 }
