99c132ff42caf7737044b6bbd0786c4afd1a8057
Seamus Connolly <sghconnolly@gmail.com>
2018-06-22 10:54:45 +0100

    feat: Convert locations array to state

--- map-app/src/App.js
+++ map-app/src/App.js
@@ -2,17 +2,8 @@ import React from "react";
 import "./index.css";
 
 export default class App extends React.Component {
-
-
-    componentDidMount() {
-        let markers = [];
-        let map = new window.google.maps.Map(document.getElementById("map"), {
-            center: { lat: 50.7934612, lng: -1.1098803 },
-            zoom: 12,
-            mapTypeId: "roadmap"
-        });
-        // Markers
-        let locations = [
+    state = {
+        locations: [
             {title: 'Spinnaker Tower', location: {lat: 50.7954961, lng: -1.1086307}},
             {title: 'HMS Warrior 1860', location: {lat: 50.7972356, lng: -1.1092372}},
             {title: 'Round Tower', location: {lat: 50.789984, lng: -1.1064873}},
@@ -22,13 +13,23 @@ export default class App extends React.Component {
             {title: 'The D-Day Story', location: {lat: 50.7780965, lng: -1.0835397}},
             {title: 'Southsea Castle', location: {lat: 50.7792529, lng: -1.0926681}},
             {title: 'Royal Marines Museum', location: {lat: 50.7820239, lng: -1.0687221}},
-            {title: 'The Royal Navy Submarine Museum', location: {lat: 50.7858205, lng: -1.1245246}}
-        ];
+            {title: 'The Royal Navy Submarine Museum', location: {lat: 50.7881692, lng: -1.1195995}}
+        ],
+        markers: []
+    }
+
+    componentDidMount() {
+        const {locations} = this.state
+        const markersList = []
+        let map = new window.google.maps.Map(document.getElementById("map"), {
+            center: { lat: 50.7934612, lng: -1.1098803 },
+            zoom: 12,
+            mapTypeId: "roadmap"
+        });
 
         let largeInfoWindow = new window.google.maps.InfoWindow();
         let bounds = new window.google.maps.LatLngBounds();
 
-        // Create an array of markers on initialise
         for (let i = 0; i < locations.length; i++) {
             let position = locations[i].location;
             let title = locations[i].title;
@@ -38,22 +39,23 @@ export default class App extends React.Component {
                 position: position,
                 title: title,
                 animation: window.google.maps.Animation.DROP,
-                id: i
-            });
+                id: title
+            })
             // Push each marker to the markers array
-            markers.push(marker);
+            markersList.push(marker);
             // Extend the boundaries of the map for the Markers
             bounds.extend(marker.position);
             // Create an onclick event for the infowindows
             marker.addListener('click', function() {
                 populateInfoWindow(this, largeInfoWindow);
             });
+            this.setState({markers: markersList})
             map.fitBounds(bounds);
         }
         // This function populates the infowindow when a marker is clicked
         function populateInfoWindow(marker, infowindow) {
             // Ensure that the infowindow is not already open on this marker
-            if (infowindow.marker != marker) {
+            if (infowindow.marker !== marker) {
                 infowindow.marker = marker;
                 // InfoWindow content is specified here
                 infowindow.setContent('<div>' + marker.title + '</div>');
